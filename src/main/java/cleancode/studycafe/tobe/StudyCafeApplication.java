7e477e2a18bb064896167f605f3a0d682fa87b2e
moonjin-kim <ace39418@gmail.com>
2025-03-12 21:08:41 +0900

    rft: handler di 적용

--- src/main/java/cleancode/studycafe/tobe/StudyCafeApplication.java
+++ src/main/java/cleancode/studycafe/tobe/StudyCafeApplication.java
@@ -1,9 +1,17 @@
 package cleancode.studycafe.tobe;
 
+import cleancode.studycafe.tobe.io.CSVStudyCafeFileHandler;
+import cleancode.studycafe.tobe.io.ConsoleInputHandler;
+import cleancode.studycafe.tobe.io.ConsoleOutputHandler;
+
 public class StudyCafeApplication {
 
     public static void main(String[] args) {
-        StudyCafePassMachine studyCafePassMachine = new StudyCafePassMachine();
+        StudyCafePassMachine studyCafePassMachine = new StudyCafePassMachine(
+                new ConsoleInputHandler(),
+                new ConsoleOutputHandler(),
+                new CSVStudyCafeFileHandler()
+        );
         studyCafePassMachine.run();
     }
 
--- src/main/java/cleancode/studycafe/tobe/StudyCafePassMachine.java
+++ src/main/java/cleancode/studycafe/tobe/StudyCafePassMachine.java
@@ -1,9 +1,7 @@
 package cleancode.studycafe.tobe;
 
 import cleancode.studycafe.tobe.exception.AppException;
-import cleancode.studycafe.tobe.io.InputHandler;
-import cleancode.studycafe.tobe.io.OutputHandler;
-import cleancode.studycafe.tobe.io.StudyCafeFileHandler;
+import cleancode.studycafe.tobe.io.*;
 import cleancode.studycafe.tobe.model.StudyCafeLockerPass;
 import cleancode.studycafe.tobe.model.StudyCafePass;
 import cleancode.studycafe.tobe.model.StudyCafePassType;
@@ -13,9 +11,20 @@ import java.util.Optional;
 
 public class StudyCafePassMachine {
 
-    private final InputHandler inputHandler = new InputHandler();
-    private final OutputHandler outputHandler = new OutputHandler();
-    private final StudyCafeFileHandler studyCafeFileHandler = new StudyCafeFileHandler();
+    private final InputHandler inputHandler;
+    private final OutputHandler outputHandler;
+    private final StudyCafeFileHandler studyCafeFileHandler;
+
+    public StudyCafePassMachine(
+            InputHandler inputHandler,
+            OutputHandler outputHandler,
+            StudyCafeFileHandler studyCafeFileHandler
+    ) {
+        this.inputHandler = inputHandler;
+        this.outputHandler = outputHandler;
+        this.studyCafeFileHandler = studyCafeFileHandler;
+
+    }
 
     public void run() {
         try {
